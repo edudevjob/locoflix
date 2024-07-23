7636cfa53eb422df34b089fd4fc9643c56f1ecf7
edudevjob <edudeveloper@edudevelopers-MacBook-Pro.local>
2024-07-23 13:12:45 -0300

    feat: Criando a entidade Category com TDD

--- backend-admin-catalago/application/src/main/java/com/mrs/admin/catalogo/application/UseCase.java
+++ backend-admin-catalago/application/src/main/java/com/mrs/admin/catalogo/application/UseCase.java
@@ -1,6 +1,6 @@
 package com.mrs.admin.catalogo.application;
 
-import com.mrs.admin.catalogo.domain.Category;
+import com.mrs.admin.catalogo.domain.category.Category;
 
 public class UseCase {
     public Category execute(){
