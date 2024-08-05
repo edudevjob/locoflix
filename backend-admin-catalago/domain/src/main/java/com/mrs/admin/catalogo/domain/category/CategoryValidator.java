d782405f84c669e6069cae532702a73739d78cde
edudevjob <edudeveloper@edudevelopers-MacBook-Pro.local>
2024-08-05 20:12:49 -0300

    feat: implementando mais validações com TDD

--- backend-admin-catalago/domain/src/main/java/com/mrs/admin/catalogo/domain/category/CategoryValidator.java
+++ backend-admin-catalago/domain/src/main/java/com/mrs/admin/catalogo/domain/category/CategoryValidator.java
@@ -15,8 +15,22 @@ public class CategoryValidator extends Validator {
 
     @Override
     public void validate() {
-        if(this.category.getName() == null){
+        checkNameConstraints();
+    }
+
+    private void checkNameConstraints() {
+        final String name = this.category.getName();
+        if(name == null){
             this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+        if(name.isBlank()){
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+        final int length = name.trim().length();
+        if(length > 255 || length < 3){
+            this.validationHandler().append(new Error("'name' must be less than 3 end 255 characters"));
         }
     }
 }
